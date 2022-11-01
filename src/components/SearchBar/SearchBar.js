53950bf48076a992bb5832e3f85bbb4d51628527
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-01 16:20:35 -0600

    Fixed searchbar issue where links did not show up properly

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -19,7 +19,7 @@ class SearchBar extends React.Component {
         <div className='SearchBar'>
             <div className='SearchBar-sort-options'>
                 <ul>
-                    this.renderSortByOptions()
+                    {this.renderSortByOptions()}
                 </ul>
             </div>
             <div className='SearchBar-fields'>
