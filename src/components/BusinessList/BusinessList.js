f562881068ab6da43441d0850690e73b51daea02
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-01 11:51:27 -0600

    added return statement to businesslist component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,12 +4,14 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
     render() {
+        return (
         <div className='BusinessList'>
             <Business />
             <Business />
             <Business />
             <Business />
         </div>
+        )
     }
 }
 
