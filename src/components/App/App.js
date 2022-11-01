c0a8557bf1e0b6b657efcfac09a30374a25656b2
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-01 11:32:18 -0600

    added more dependencies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import BusinessList from '../BusinessList/BusinessList.js'
+import SearchBar from '../SearchBar/SearchBar.js'
 
 function App() {
   return (
