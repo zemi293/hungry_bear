7116d5014432cead80e7306f7017784444596c81
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-01 11:40:57 -0600

    Added content to App Function Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import BusinessList from '../BusinessList/BusinessList'
-import SearchBar from '../SearchBar/SearchBar'
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(); {
+    return (
+      <div className='App'>
+        <h1>hungry bear</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    )
+  } 
 }
 
 export default App;
