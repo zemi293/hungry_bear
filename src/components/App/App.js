2e3d006026459f36e48aaf1b207af92c303fcb8a
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-04 12:25:33 -0600

    business object was moved?

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,18 @@ import React from 'react';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
 class App extends React.Component {
   render() {
     return (
