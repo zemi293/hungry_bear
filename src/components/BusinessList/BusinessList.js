33e3217071cc21616e880cf373dd568226b90e22
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-04 12:41:31 -0600

    same thing?

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,7 +7,9 @@ class BusinessList extends React.Component {
         return (
         <div className='BusinessList'>
             {
-            this.props.businesses.map()
+            this.props.businesses.map((business) => {
+                return <Business business = {business}/>
+            })
             }
         </div>
         )
