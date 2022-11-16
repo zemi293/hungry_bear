d0f0ddf1d64aeb32b08f51721616fa96d74a1e96
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-16 10:58:00 -0700

    reformatted api key object

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,10 @@
 const apiKey = process.env.REACT_APP_API_KEY;
 const Yelp = {
     search(term, location, sortBy) {
-      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, headers: `Bearer ${apiKey}`);
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+                }
+        });
     }
 };
