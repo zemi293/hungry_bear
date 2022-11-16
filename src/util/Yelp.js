1dcb855083877ae1bc684c86dbc9fef47c91cdbb
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-16 10:50:25 -0700

    added cors

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,6 @@
 const apiKey = process.env.REACT_APP_API_KEY;
 const Yelp = {
     search(term, location, sortBy) {
-      return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
     }
 };
