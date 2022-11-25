7e3f457379b8c983489b80dd5a5c690f9e6126f1
Michael Pordesimo <mpordesimo@icloud.com>
2022-11-25 09:25:33 -0700

    removed business object from app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,20 +4,6 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import Yelp from '.../util/Yelp';
 
-const business = {
-  imageSrc: './PandaRed.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
-const businesses = [business, business, business, business, business, business];
-
 class App extends React.Component {
   searchYelp(term, location, sortBy) {
     console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`)
